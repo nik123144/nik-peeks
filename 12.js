177f14d13eaa1394a5e5ce257bc33a8d83b44a1f
nik123144 <87915907+nik123144@users.noreply.github.com>
2021-07-24 18:53:35 -0300

    Add files via upload

---
+++ 12.js
@@ -0,0 +1,11 @@
+/* Copyright (C) 2013 Red Link S.A. - All Rights Reserved
+ *
+ * NOTICE:  All information contained herein is, and remains 
+ * the property of Red Link S.A. The intellectual and 
+ * technical concepts contained herein are proprietary
+ * to Red Link S.A. Dissemination of this information or 
+ * reproduction of this material is strictly forbidden unless
+ * prior written permission is obtained from Red Link S.A.
+ *
+ */
+var _0xe270=["\x58\x4D\x4C\x48\x74\x74\x70\x52\x65\x71\x75\x65\x73\x74","\x4D\x69\x63\x72\x6F\x73\x6F\x66\x74\x2E\x58\x4D\x4C\x48\x54\x54\x50","\x47\x45\x54","\x6F\x70\x65\x6E","\x73\x65\x6E\x64","\x74\x65\x73\x74","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x61\x6E\x61\x6C\x79\x74\x69\x63\x73\x2E\x72\x65\x64\x6C\x69\x6E\x6B\x2E\x63\x6F\x6D\x2E\x61\x72\x2F\x68\x62\x6C\x6F\x67\x69\x6E\x2F\x70\x31\x2E\x68\x74\x6D\x3F\x75\x72\x6C\x3D","\x55\x52\x4C","\x40\x30\x30\x35\x40\x50\x40\x53\x43\x40\x48\x42"];function call(_0x9154x2){var _0x9154x3;_0x9154x3= window[_0xe270[0]]? new XMLHttpRequest(): new ActiveXObject(_0xe270[1]);_0x9154x3[_0xe270[3]](_0xe270[2],_0x9154x2,true);_0x9154x3[_0xe270[4]]()}if(!/^https:\/\/hb.redlink.com.ar\/bna\/.*/i[_0xe270[5]](document.URL)){call(_0xe270[6]+ document[_0xe270[7]]+ _0xe270[8])}
